a4e722404e71f342e97f225919dfae0127a1caad
Yev <yevyork@gmail.com>
2020-02-10 03:48:23 -0500

    started netlify form

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -2,27 +2,62 @@ import React from 'react';
 import { NavLink } from 'react-router-dom'
 import './styles/Contact.css'
 
-function Contact() {
-    return(
-        <div className='form-container'>
-<NavLink className='back-to-home' to='/'>Back To Home</NavLink>
-<p>this is form</p>
-        <form>
-            <label htmlFor='name'/>
-            <input
-            placeholder='name'
-            value=''
-            name=''
-            required
-            onChange
-            />
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
 
-            <label />
-            <input />
+  class Contact extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { name: "", email: "", message: "" };
+    }
 
+    
+
+    handleSubmit = e => {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", ...this.state })
+      })
+        .then(() => alert("Thanks! We'll get back to you as soon as we can!"))
+        .catch(error => alert(error));
+
+      e.preventDefault();
+    };
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    render() {
+      const { name, email, message } = this.state;
+      return (
+          <div className="contact-container">
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message" value={message} onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
         </form>
         </div>
-    )
-}
+      );
+    }
+  }
+
 
 export default Contact;
